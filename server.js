fec45636ad1b179092ad00966fdb384048eb5060
ogheneruno <orunosaduwa@gmail.com>
2019-11-22 17:25:01 +0100

    created all models

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/database');
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
+app.use(cors());
 
 
+// ----------------- DB Connection ----------------------
+connectDB();
 
 
 
